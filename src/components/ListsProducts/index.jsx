cfca11355d50551be7971137798d785d8d0df75b
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-02-29 18:34:13 -0300

    add func filter

---
+++ src/components/ListsProducts/index.jsx
@@ -0,0 +1,69 @@
+import SubTitle from "../Subtitle";
+import Button from "../Button";
+import Product from "../Product";
+import list from "../Product/list.json";
+import { useState } from "react";
+
+const ListsProducts = () => {
+  const [products, setProducts] = useState(list);
+
+  const filterSession = (session) => {
+    if (session === "All Products") {
+      setProducts(list);
+    } else {
+      const filteredProducts = list.filter((item) => item.session === session);
+      setProducts(filteredProducts);
+    }
+  };
+
+  return (
+    <>
+      <div className="subtitle-products">
+        <SubTitle subtitle="🥗Farm fresh products" />
+        <div className="products-sessions">
+          <Button
+            text="All Products"
+            className="pink"
+            onClick={() => filterSession("All Products")}
+          />
+          <Button
+            text="Fruits"
+            className="transparent"
+            onClick={() => filterSession("Fruits")}
+          />
+          <Button
+            text="Vegetables"
+            className="transparent"
+            onClick={() => filterSession("Vegetables")}
+          />
+          <Button
+            text="Bakery"
+            className="transparent"
+            onClick={() => filterSession("Bakery")}
+          />
+          <Button
+            text="Milk"
+            className="transparent"
+            onClick={() => filterSession("Milk")}
+          />
+        </div>
+      </div>
+      <div className="products">
+        {products.map((item, index) => (
+          <Product
+            key={index}
+            highlight={item.highlight}
+            src={item.src}
+            title={item.title}
+            stars={item.stars}
+            session={item.session}
+            price={item.price}
+            classColor={item.classColor}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ListsProducts;
