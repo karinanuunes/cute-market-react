bad35b59acb3427e797644dc2008fd453b2956c4
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-02-28 23:13:49 -0300

    add button cards

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,14 @@ function App() {
           src="https://images.vexels.com/media/users/3/295123/isolated/lists/d95cb8b88f754d47800f01844fe0717a-legumes-de-banana-kawaii.png"
           alt="Banana Kawaii"
           classColor="green"
+          classButtonColor="peach"
         />
         <Card
           title="Make your breakfast healthy and easy"
           src="https://images.vexels.com/media/users/3/295127/isolated/lists/fbb7a506340ecbfd3e2f61d5383c13f5-legumes-de-abacate-kawaii.png"
           alt="Abacate Kawaii"
           classColor="orange"
+          classButtonColor="yellow"
         />
       </div>
     </>
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,16 +1,23 @@
 import PropTypes from "prop-types";
 import "./index.css";
+import Button from "../Button";
 
-const Card = ({ classColor, title, src }) => {
+const Card = ({ classButtonColor, classColor, title, src }) => {
   return (
     <div className={`card ${classColor}`}>
-      <h3>{title}</h3>
-      <img src={src} alt={title} />
+      <div className="card-title">
+        <h3>{title}</h3>
+      </div>
+      <div className="card-image">
+        <img src={src} alt={title} />
+      </div>
+      <Button text="Show Now" className={`shop ${classButtonColor}`} />
     </div>
   );
 };
 
 Card.propTypes = {
+  classButtonColor: PropTypes.string,
   classColor: PropTypes.string,
   title: PropTypes.string,
   src: PropTypes.string,
