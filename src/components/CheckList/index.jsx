2683f9978ce6462085a9bb33ff5cd74ba107da9b
Karina Nunes <karina.nuunes@yahoo.com.br>
2024-03-07 21:31:51 -0300

    ajust checklist

--- src/components/CheckList/index.jsx
+++ src/components/CheckList/index.jsx
@@ -2,6 +2,7 @@ import { useState, useReducer } from "react";
 import SubTitle from "../Subtitle";
 import Button from "../Button";
 import "./checklist.css";
+import list from "../Product/list.json";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -29,35 +30,45 @@ const CheckList = () => {
 
   return (
     <div className="checklist">
-      <SubTitle subtitle="CheckList" />
-      <p>Make a checklist of what you need</p>
-      <div className="checklist-input">
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <Button
-          onClick={() => {
-            dispatch({ type: "add-items", payload: inputValue });
-            setInputValue("");
-          }}
-          className="orange"
-          text="Add items"
-        />
+      <div className="card pink checklist-client">
+        <SubTitle subtitle="CheckList" />
+        <p>Make a checklist of what you need</p>
+        <div className="checklist-input">
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <Button
+            onClick={() => {
+              dispatch({ type: "add-items", payload: inputValue });
+              setInputValue("");
+            }}
+            className="blue"
+            text="Add items"
+          />
+        </div>
+        <div className="checklist-items">
+          <ul>
+            {state.items.map((task, index) => (
+              <li
+                key={index}
+                onClick={() => dispatch({ type: "risk-items", payload: index })}
+                style={{
+                  textDecoration: task.isCompleted ? "line-through" : "none",
+                }}
+              >
+                {task.name}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="checklist-items">
+      <div className="card yellow checklist-card">
+        <SubTitle subtitle="CheckList Ideas" />
         <ul>
-          {state.items.map((task, index) => (
-            <li
-              key={index}
-              onClick={() => dispatch({ type: "risk-items", payload: index })}
-              style={{
-                textDecoration: task.isCompleted ? "line-through" : "none",
-              }}
-            >
-              {task.name}
-            </li>
+          {list.map((product, id) => (
+            <li key={id}>{product.title}</li>
           ))}
         </ul>
       </div>
